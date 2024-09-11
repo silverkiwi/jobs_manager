21bccd029046141cf80c6c0c17c559e38d08650f
Corrin Lakeland <corrin.lakeland@countdown.co.nz>
2024-09-11 16:36:27 +1200

    Code hygene

--- jobs_manager/settings.py
+++ jobs_manager/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "workflow",
+    #    "workflow.apps.WorkflowConfig",  # Random bug - changing from "workflow" to this as a hacky workaround
     "simple_history",
 ]
 
--- workflow/tests.py
+++ workflow/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+import django
+
+django.setup()  # Force initialization
+
+
+class SimpleTest(TestCase):
+    def test_basic(self):
+        self.assertTrue(True)
