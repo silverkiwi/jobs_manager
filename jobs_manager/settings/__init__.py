f91fe6e2febcbf28ca80c02551ebe75009f3f324
FernandoAurelius <miguelfernandoaurelius@gmail.com>
2025-01-18 23:16:54 -0300

    feat(settings): add redis cache configuration for AWS EC2 instances feat(settings): add AWS environment variable to determine if the app is running on AWS feat(settings): import cache settings if the app is running on AWS feat(pyproject.toml): add django-redis dependency

--- jobs_manager/settings/__init__.py
+++ jobs_manager/settings/__init__.py
@@ -7,8 +7,13 @@ from .base import BASE_DIR
 
 load_dotenv(BASE_DIR / ".env")
 ENVIRONMENT = os.getenv("DJANGO_ENV", "local")
+AWS = os.getenv("AWS", False)
 
 if ENVIRONMENT == "production_like":
     from .production_like import *
+
+    # If the app is running on an EC2 instance on AWS, then import different cache settings
+    if AWS:
+        from .cache import *
 else:
     from .local import *
