01bb23dc4aae87d0aaf347fc3b6ca9d709826ad1
FernandoAurelius <miguelfernandoaurelius@gmail.com>
2025-03-17 20:49:54 -0300

    fix(xero_view.py): change create_document to delete_document to actually delete the document

--- workflow/views/xero_view.py
+++ workflow/views/xero_view.py
@@ -797,7 +797,7 @@ def delete_xero_quote(request: HttpRequest, job_id: uuid) -> HttpResponse:
     try:
         job = Job.objects.get(id=job_id)
         creator = XeroQuoteCreator(job)
-        response = json.loads(creator.create_document().content.decode())
+        response = json.loads(creator.delete_document().content.decode())
 
         if not response.get("success"):
             messages.error(request, f"Failed to delete quote: {response.get("error")}")
