37eb59dca0ba2a20b6e27e12e9ac507e82ab3d51
FernandoAurelius <miguelfernandoaurelius@gmail.com>
2025-05-09 09:13:17 -0300

    refactor(migrations): remove redundant and outdated migration files
    The migration files `0135_merge_20250508_2123.py` and
    `0136_alter_historicaljob_status.py` are removed because they are
    either merge artifacts or contain changes that are no longer relevant
    or have been superseded by other migrations. Also, remove
    dependencies to removed files.

--- workflow/migrations/0132_stock_job_stock_source_purchase_order_line.py
+++ workflow/migrations/0132_stock_job_stock_source_purchase_order_line.py
@@ -4,9 +4,6 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ("workflow", "0131_aiprovider_companydefaults_anthropic_api_key_and_more"),
-        ("workflow", "0132_job_people_staff_icon_and_more"),
-        ("workflow", "0133_rename_job_status"),
-        ("workflow", "0134_auto_20250508_2012"),
     ]
 
     operations = []
