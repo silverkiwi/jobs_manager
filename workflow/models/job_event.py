06227a6f24494e44152614a70dfb5cd6049ff555
FernandoAurelius <miguelfernandoaurelius@gmail.com>
2025-05-15 17:59:12 -0300

    feat(job): add job app
    This commit introduces a new app called "job" to manage job-related data.  It includes models for jobs, job events, job files, and job pricing, along with admin configurations and tests.  This improves the organization and management of job information within the application.

--- workflow/models/job_event.py
+++ workflow/models/job_event.py
@@ -3,15 +3,9 @@ from django.utils.timezone import now
 
 from accounts.models import Staff
 
+from job.models import JobEvent as BaseJobEvent
 
-class JobEvent(models.Model):
-    job = models.ForeignKey("Job", on_delete=models.CASCADE, related_name="events")
-    timestamp = models.DateTimeField(default=now)
-    staff = models.ForeignKey(Staff, on_delete=models.SET_NULL, null=True, blank=True)
-    event_type = models.CharField(
-        max_length=100, null=False, blank=False, default="automatic_event"
-    )  # e.g., "status_change", "manual_note"
-    description = models.TextField()
 
-    def __str__(self):
-        return f"{self.timestamp}: {self.event_type} for {self.job.name}"
+class JobEvent(BaseJobEvent):
+    class Meta:
+        proxy = True
