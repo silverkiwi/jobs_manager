df54ebf93776dbdbdb40a22979e43b3537ad260d
FernandoAurelius <miguelfernandoaurelius@gmail.com>
2025-06-04 12:41:42 -0300

    fix(migrations): change related_name to workflow.purchaseorder
    The related_name in the ForeignKey was pointing to purchasing.purchaseorder,
    which is incorrect. It should point to workflow.purchaseorder. This commit
    corrects the related_name to ensure that the ForeignKey relationship is
    properly established.

--- apps/workflow/migrations/0122_companydefaults_anthropic_api_key_and_more.py
+++ apps/workflow/migrations/0122_companydefaults_anthropic_api_key_and_more.py
@@ -60,7 +60,7 @@ class Migration(migrations.Migration):
                     models.ForeignKey(
                         on_delete=django.db.models.deletion.CASCADE,
                         related_name="quotes",
-                        to="purchasing.purchaseorder",
+                        to="workflow.purchaseorder",
                     ),
                 ),
             ],
